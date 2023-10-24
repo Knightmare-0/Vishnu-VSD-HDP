d8357bd5dcc261ac89e9b87856afae6ad1879ad3
Knight <vishnuvidyesh.77@gmail.com>
2023-10-24 14:08:58 +0530

    Clock frequency

--- RTL2GDS/contraint.tcl
+++ RTL2GDS/contraint.tcl
@@ -5,8 +5,8 @@ set_clock_uncertainty -setup 0.5 [get_clocks MYCLK];
 set_clock_uncertainty -hold 0.1 [get_clocks MYCLK];
 set_input_delay -max 3 -clock [get_clocks MYCLK] [get_ports ctrl];
 set_input_delay -min 1 -clock [get_clocks MYCLK] [get_ports ctrl];
-set_input_transition -max 0.4 [get_ports ctrl];
+set_input_transition -max 0.5 [get_ports ctrl];
 set_input_transition -min 0.1 [get_ports ctrl];
-set_load -max 0.4 [get_ports count];
-set_load -min 0.1 [get_ports count];
+set_load -max 0.1 [get_ports count];
+set_load -min 0.01 [get_ports count];
 
